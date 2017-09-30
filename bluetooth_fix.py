bd374b187672d867e1e066a84b9ab51ef9c83ac3
Mark Winterbottom <mark@londonappdeveloper.com>
2017-09-30 15:44:05 +0100

    Updated.

---
+++ bluetooth_fix.py
@@ -0,0 +1,75 @@
+import configparser
+import argparse
+
+
+def _parse_args():
+    """Parse arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--reg_path', help='Path to reg file.', default='keys.reg')
+
+    return parser.parse_args()
+
+
+def _open_reg_file(file_path):
+    """ Open file at given path and return as config option."""
+    config = configparser.ConfigParser()
+    config.read_file(open(file_path))
+
+    return config
+
+
+def _insert_mac_colons(mac):
+    """ Bluetooth Mac directory file name."""
+    mac = mac.upper()
+    mac_parts = [mac[i:i + 2] for i in range(0, len(mac), 2)]
+    # import pdb; pdb.set_trace()
+    return ':'.join(mac_parts)
+
+
+def _bluetooth_dir_name(section_name):
+    """ Return the bluetooth directory name."""
+    full_path = section_name.split('\\')
+    last_two_macs = full_path[-2:]
+    path_parts = []
+    for mac in last_two_macs:
+        path_parts.append(_insert_mac_colons(mac))
+
+    return '/'.join(path_parts)
+
+
+def _format_ltk(ltk):
+    """ Convert LTK to uppercase and remove commas."""
+    return ltk.lstrip('hex:').upper().replace(',', '')
+
+
+def _process_reg_file(config):
+    """ Process the reg file."""
+    sections = config.sections()
+    for section in sections:
+        if len(config[section]) != 10:
+            continue
+        print('\n')
+        print('Dir Name: /usr/lib/bluetooth/{}'.format(
+            _bluetooth_dir_name(section)))
+        print('LongTermKey: {}'.format(_format_ltk(config[section]['LTK'])))
+        print('\n====================================\n')
+
+
+def main():
+    """ Main entrypoint to script. """
+    args = _parse_args()
+    config = _open_reg_file(args.reg_path)
+    _process_reg_file(config)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# reg_str = reg_file_to_str('/home/mark/Desktop/BTKeys.reg')
+# file_path_to_dict(reg_str)
+
+
+
+# Print
--- clean_reg_file.py
+++ clean_reg_file.py
@@ -19,6 +19,19 @@ def _reg_file_to_str(file_path):
         return file_str
 
 
+def _clean(contents):
+    """ Clean contents and return."""
+    contents = contents.replace('"', '')
+    lines = contents.split('\r\n')
+    del lines[0]
+    new_lines = []
+    for line in lines:
+        new_lines.append(line.strip())
+
+    return '\n'.join(new_lines)
+
+
+
 def _save_str_to_file(contents, file_path):
     """Save the string as a file."""
     with open(file_path, 'wb') as f:
@@ -29,6 +42,7 @@ def main():
     """Main entrypoint to script."""
     args = _parse_args()
     file_contents = _reg_file_to_str(args.file_path)
+    file_contents = _clean(file_contents)
     _save_str_to_file(file_contents, args.output)
 
 
